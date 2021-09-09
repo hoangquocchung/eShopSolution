c1e4e068b8939fa051ad5426929ad934653b77d1
hoangquocchung <hoangchung0123456789@gmail.com>
2021-09-09 21:53:48 +0700

    Add cookie authentication without identity

--- eShopSolution.AdminApp/Controllers/HomeController.cs
+++ eShopSolution.AdminApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using eShopSolution.AdminApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,17 +10,21 @@ using System.Threading.Tasks;
 
 namespace eShopSolution.AdminApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
+        
         public HomeController(ILogger<HomeController> logger)
         {
+           
             _logger = logger;
         }
 
         public IActionResult Index()
-        {
+        { 
+            var user = User.Identity.Name;
             return View();
         }
 
---
+++ eShopSolution.AdminApp/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        //private readonly IUserApiClient _userApiClient;
+        //private readonly IConfiguration _configuration;
+
+        //public LoginController(IUserApiClient userApiClient,
+        //    IConfiguration configuration)
+        //{
+        //    _userApiClient = userApiClient;
+        //    _configuration = configuration;
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
