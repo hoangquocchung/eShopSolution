cf9c9615f3924e320b64f5babb1a8e5eac09169e
hoangquocchung <hoangchung0123456789@gmail.com>
2021-10-08 12:11:33 +0700

    add cartitem

--- eShopSolution.WebApp/Startup.cs
+++ eShopSolution.WebApp/Startup.cs
@@ -100,7 +100,7 @@ namespace eShopSolution.WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
 
