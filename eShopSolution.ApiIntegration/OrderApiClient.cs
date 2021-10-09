e7c3901f765e1f85727ed68aefdf85e0110e7d2f
hoangquocchung <hoangchung0123456789@gmail.com>
2021-10-09 21:52:35 +0700

    check out giỏ hàng

---
+++ eShopSolution.ApiIntegration/OrderApiClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.ApiIntegration
+{
+    public class OrderApiClient : BaseApiClient, IOrderApiClient
+    {
+        public OrderApiClient(IHttpClientFactory httpClientFactory,
+                   IHttpContextAccessor httpContextAccessor,
+                    IConfiguration configuration)
+            : base(httpClientFactory, httpContextAccessor, configuration)
+        {
+        }
+    }
+}
